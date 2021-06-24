95fc0ed1af3ffd8346f5bc7d4cb52ca753eb35a8
bhavesh-gohad <78643007+bhavesh-gohad@users.noreply.github.com>
2021-06-24 23:42:59 +0530

    Add files via upload

---
+++ SequenceEquation.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	
+	int index1=0,index2=0,x=0;
+	
+	int a[n];
+	
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	
+	while(++x && x<=n)
+	{
+		for(int i=0;i<n;i++)
+		{
+			if(a[i] == x)
+			{
+				index1 = i+1;
+				
+				for(int k=0;k<n;k++)
+				{
+					if(a[k] == index1)
+					{
+						cout<<k+1<<endl;
+						break;
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
